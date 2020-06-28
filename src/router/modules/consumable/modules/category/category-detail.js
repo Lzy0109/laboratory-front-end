0e343205ee183228ff42876532d347107c6e8a98
lgy <Lguoye@qq.com>
2020-06-28 11:28:53 +0800

    添加耗材种类相关页面，修复种类详情页路由指向问题

--- src/router/modules/consumable/modules/category/category-detail.js
+++ src/router/modules/consumable/modules/category/category-detail.js
@@ -2,7 +2,7 @@
 const consumableCategoryDetailRouter = {
   path: 'category_detail',
   component: () => import('@/views/laboratory_1/consumable/category/category-detail'),
-  name: 'Category_Detail',
+  name: 'Consumable_Category_Detail',
   meta: { title: '耗材分类详情'},
   hidden: true
 }
