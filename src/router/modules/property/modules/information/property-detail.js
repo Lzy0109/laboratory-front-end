3ae301957f64f2fcc2d0005345c7c4fb50fdfab4
Lzy0109 <752867914@qq.com>
2020-06-29 22:46:08 +0800

    资产管理和详情页面

--- src/router/modules/property/modules/information/property-detail.js
+++ src/router/modules/property/modules/information/property-detail.js
@@ -2,7 +2,7 @@
 const propertyDetailRouter = {
   path: 'property_detail',
   component: () => import('@/views/laboratory_1/property/property/information/property-detail'),
-  name: 'property_detail',
+  name: 'Property_Detail',
   meta: { title: '资产出借信息详情' },
   hidden: true
 }
